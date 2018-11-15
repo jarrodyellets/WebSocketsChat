2023320647f01f88fab7c9a78378afc0a08c1c10
jarrodyellets <jarrod524@gmail.com>
2018-11-15 11:38:59 +0100

    Added broadcast messages

--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,8 @@ const socket = io.connect('http://localhost:4000');
 const message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
-      output = document.getElementById('output');
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
 //emit events
 btn.addEventListener('click', function(){
@@ -15,7 +16,16 @@ btn.addEventListener('click', function(){
   });
 });
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+})
+
 //listen for events
 socket.on('chat', function(data){
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+  feedback.innerHTML = '<p><em></em></p>';
+})
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing...</em></p>';
 })
