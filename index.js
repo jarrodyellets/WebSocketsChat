28c277a33aff5b9edddfd15af991bc5898c477e8
jarrodyellets <jarrod524@gmail.com>
2018-11-15 11:06:54 +0100

    Loaded socket.io

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const server = app.listen(4000, function(){
   console.log("Server is running on port 4000");
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+//Socket setup
+const io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('made socket connection', socket.id)
+})
